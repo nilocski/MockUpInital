9bdedad388c40cf79853e73a09b7212690db42c7
Colin Mackenzie <nilocski@yahoo.com>
2016-10-08 14:29:20 +0200

    Initial Commit

---
+++ MockUpInital/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MockUpInital
+//
+//  Created by Colin Mackenzie on 08/10/2016.
+//  Copyright © 2016 cdmackenzie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
